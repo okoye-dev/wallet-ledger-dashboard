2a7ad57b0bb11de162a16e1a367cae3bed29cdee
okoye-dev <okoyedivine145@gmail.com>
2025-08-08 16:20:57 +0100

    (final): date col span update

--- components/dashboard/table/config.ts
+++ components/dashboard/table/config.ts
@@ -30,7 +30,7 @@ export const TABLE_HEADERS: TableHeaderConfig[] = [
   {
     field: "date",
     label: "Date",
-    width: "md:w-[60%]",
+    width: "md:w-[55%]",
     padding: TABLE_STYLES.cellPadding.first,
     textAlign: "text-left",
     sortable: true,
